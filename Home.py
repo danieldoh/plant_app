0503dcc068cbd0ab101d872ee91d2153c0cce8d2
danieldoh <doh7337@gmail.com>
2024-06-12 11:19:14 +0900

    Displayed the image in streamlit

--- Home.py
+++ Home.py
@@ -1,3 +1,14 @@
 import streamlit as st
+import cv2
+import numpy as np
+from PIL import Image
 
 st.title("Hello")
+
+def convert_opencv_to_pil(img):
+    return Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
+img = cv2.imread('./image/camera_1/20240531_102924.jpg')
+
+st.image(convert_opencv_to_pil(img), caption='Clikc on the image to get pixel value')
+
