11e0c3b7d585229469f960710fa71649888dc293
danieldoh <doh7337@gmail.com>
2024-06-17 11:56:54 +0900

    Organized the code with functions

--- pages/01_Trunk_Analysis.py
+++ pages/01_Trunk_Analysis.py
@@ -51,9 +51,111 @@ if 'angle' not in st.session_state:
 if 'leaf_angle' not in st.session_state:
     st.session_state['leaf_angle'] = [[0,0]]
 
+
+def two_points_calculation(value, ratio, mode, mode_selected, calculated_values):
+    reset = st.button("RESET")
+    message_reset = st.empty()
+
+    if reset:
+        st.session_state[mode] = []
+        st.session_state[calculated_values][mode] = 0.0
+        message_reset.write("Resetting")
+
+        time.sleep(2)
+
+        message_reset.empty()
+        reset = False
+
+    if value and not reset:
+        if len(st.session_state[mode]) < 3:
+            st.session_state[mode].append([value['x'], value['y']])
+
+        if len(st.session_state[mode]) == 3:
+            pix_1 = st.session_state[mode][-2]
+            pix_2 = st.session_state[mode][-1]
+            st.session_state[calculated_values][mode] = two_points_distance(pix_1, pix_2) * ratio
+            st.write(f"{mode_selected}: ", f"{st.session_state[calculated_values][mode]} m")
+            st.write(f"{mode_selected} calculation is finished. Please press the RESET button to select new points.")
+
+        if len(st.session_state[mode]) > 1:
+            st.write("Number of Pixel Clicked: ", len(st.session_state[mode])-1)
+            st.write("Points Selected: ", st.session_state[mode][1:])
+
+def three_points_angle_calculation(value, mode, mode_selected, calculated_values):
+    reset = st.button("RESET")
+    message_reset = st.empty()
+
+    if reset:
+        st.session_state[mode] = []
+        st.session_state[calculated_values][mode] = 0.0
+        message_reset.write("Resetting")
+
+        time.sleep(2)
+
+        message_reset.empty()
+        reset = False
+
+    if value and not reset:
+        if len(st.session_state[mode]) < 4:
+            st.session_state[mode].append([value['x'], value['y']])
+
+        if len(st.session_state[mode]) == 4:
+            pix_1 = st.session_state[mode][-3]
+            pix_2 = st.session_state[mode][-2]
+            pix_3 = st.session_state[mode][-1]
+            st.session_state[calculated_values][mode] = angle_calculation(pix_1, pix_2, pix_3) 
+            st.write(f"{mode_selected}: ", f"{st.session_state[calculated_values][mode]} degrees")
+            st.write(f"{mode_selected} calculation is finished. Please press the RESET button to select new points.")
+
+        if len(st.session_state[mode]) > 1:
+            st.write("Number of Pixel Clicked: ", len(st.session_state[mode])-1)
+            st.write("Points Selected: ", st.session_state[mode][1:])
+
+def points_calculation(value, ratio, mode, mode_selected, calculated_values):
+    reset = st.button("RESET")
+    start = st.button("START")
+    end = st.button("END")
+
+    message_reset = st.empty()
+
+    if start:
+        st.session_state[mode] = []
+        st.session_state[calculated_values][mode] = 0.0
+        st.write("👉 Started.")
+
+    if reset:
+        st.session_state[mode] = []
+        st.session_state[calculated_values][mode] = 0.0
+        message_reset.write("Resetting")
+
+        time.sleep(2)
+
+        message_reset.empty()
+        reset = False
+        start = False
+        end = False
+
+    if value and not reset:
+        if not end:
+            st.session_state[mode].append([value['x'], value['y']])
+
+        if end:
+            st.write("🔚 Ended.")
+            for i in range(1, len(st.session_state[mode])-1):
+                pix_1 = st.session_state[mode][i]
+                pix_2 = st.session_state[mode][i+1]
+                st.session_state[calculated_values][mode] += two_points_distance(pix_1, pix_2)
+            st.session_state[calculated_values][mode] *= ratio
+            st.write(f"{mode_selected}: ", f"{st.session_state[calculated_values][mode]} m")
+            st.write(f"{mode_selected} calcuation is finished. Please press the RESET button to select new points.")
+
+        if len(st.session_state[mode]) > 1:
+            st.write("Number of Pixel Clicked: ", len(st.session_state[mode])-1)
+            st.write("Points Selected: ", st.session_state[mode][1:])
+
 # Image path
 uploadted_file = st.file_uploader("Choose an image file to be analyzed", type=["jpg", "jpeg", "png"])
-
+ratio = 0.00057
 
 if uploadted_file is not None:
     img_pil = Image.open(io.BytesIO(uploadted_file.read()))
@@ -70,49 +172,23 @@ if uploadted_file is not None:
     st.write(f"Tilt Angle: {st.session_state['calculated_values']['angle']} degrees")
     st.write(f"Leaf Tilt Angle: {st.session_state['calculated_values']['leaf_angle']} degrees")
 
-    mode = st.selectbox("Select the category", ["Width", "Cumulative Height", "Height", "Tilt Angle", "Leaf Tilt Angle"], index = None, placeholder="Select an option")
+    mode_selected = st.selectbox("Select the category", ["Width", "Cumulative Height", "Height", "Tilt Angle", "Leaf Tilt Angle"], index = None, placeholder="Select an option")
 
-    with st.sidebar:
-        st.write(st.session_state['width'])
-    
 
-    if mode == "Width":
+    if mode_selected == "Width":
         st.session_state['angle'] = []
         st.session_state['height'] = []
         st.session_state['cum_height'] = []
         st.session_state['leaf_angle'] = []
 
         st.write("🖐️ Click on the image to select the two points for the trunk.")
-        width_reset = st.button("RESET")
-        message_width_reset = st.empty()
 
-        if width_reset:
-            st.session_state['width'] = []
-            message_width_reset.write("Resetting")
+        # value = streamlit_image_coordinates
+        # mode = 'width'
+        # mode_selected = 'Width'
+        two_points_calculation(value, ratio, 'width', mode_selected, 'calculated_values')
 
-            time.sleep(2)
-
-            message_width_reset.empty()
-            width_reset = False
-
-        if value and not width_reset:
-
-            if len(st.session_state['width']) < 3:
-                st.session_state['width'].append([value['x'], value['y']])
-
-            if len(st.session_state['width']) == 3:
-                pix_1 = st.session_state['width'][-2]
-                pix_2 = st.session_state['width'][-1]
-                st.session_state['calculated_values']['width'] = two_points_distance(pix_1, pix_2)
-                st.write("Trunk Width: ", f"{st.session_state['calculated_values']['width'] * 0.00057} m")
-                st.write("Width calculation is finished. Please press the RESET button to select new points.")
-
-            if len(st.session_state['width']) > 1:
-                st.write("Number of Pixel Clicked: ", len(st.session_state['width'])-1)
-                st.write("Points Selected: ", st.session_state['width'][1:])
-
-
-    elif mode == "Cumulative Height":
+    elif mode_selected == "Cumulative Height":
         st.session_state['angle'] = []
         st.session_state['width'] = []
         st.session_state['height'] = []
@@ -121,44 +197,14 @@ if uploadted_file is not None:
         st.write("🖐️ Click on the image to select the side of the trunk for the cumulative height.")
         st.write("🖐️ Press START button and click the points. When you finish, press END button")
 
-        cum_height_reset = st.button("RESET")
-        cum_height_start = st.button("START")
-        cum_height_end = st.button("END")
+        # value = streamlit_image_coordinates
+        # mode = 'cum_height'
+        # mode_selected = 'Cumulative Height'
 
-        message_cum_height_reset = st.empty()
+        points_calculation(value, ratio, 'cum_height', mode_selected, 'calculated_values')
+        
 
-        if cum_height_start:
-            st.session_state['cum_height'] = []
-
-        if cum_height_reset:
-            st.session_state['cum_height'] = []
-            message_cum_height_reset.write("Resetting")
-
-            time.sleep(2)
-
-            message_cum_height_reset.empty()
-            cum_height_reset = False
-            cum_height_start = False
-            cum_height_end = False
-
-        if value and not cum_height_reset:
-
-            if not cum_height_end:
-                st.session_state['cum_height'].append([value['x'], value['y']])
-
-            if cum_height_end:
-                for i in range(1, len(st.session_state['cum_height'])-1):
-                    pix_1 = st.session_state['cum_height'][i]
-                    pix_2 = st.session_state['cum_height'][i+1]
-                    st.session_state['calculated_values']['cum_height'] += two_points_distance(pix_1, pix_2)
-                st.write("Trunk Cumulative Height: ", f"{st.session_state['calculated_values']['cum_height'] * 0.00057} m")
-                st.write("Cumulative height calcuation is finished. Please press the RESET button to select new points.")
-
-            if len(st.session_state['cum_height']) > 1:
-                st.write("Number of Pixel Clicked: ", len(st.session_state['cum_height'])-1)
-                st.write("Points Selected: ", st.session_state['cum_height'][1:])
-
-    elif mode == "Height":
+    elif mode_selected == "Height":
         st.session_state['angle'] = []
         st.session_state['width'] = []
         st.session_state['cum_height'] = []
@@ -166,36 +212,13 @@ if uploadted_file is not None:
 
         st.write("🖐️ Click on the image to select one bottom point and one top point for the height.")
 
-        height_reset = st.button("RESET")
-        message_height_reset = st.empty()
-
-        if height_reset:
-            st.session_state['height'] = []
-            message_height_reset.write("Resetting")
-
-            time.sleep(2)
-
-            message_height_reset.empty()
-            height_reset = False
-
-        if value and not height_reset:
-
-            if len(st.session_state['height']) < 3:
-                st.session_state['height'].append([value['x'], value['y']])
-
-            if len(st.session_state['height']) == 3:
-                pix_1 = st.session_state['height'][-2]
-                pix_2 = st.session_state['height'][-1]
-                st.session_state['calculated_values']['height'] = two_points_distance(pix_1, pix_2)
-                st.write("Trunk Height: ", f"{st.session_state['calculated_values']['height'] * 0.00057} m")
-                st.write("Height calcuation is finished. Please press the RESET button to select new points.")
-
-            if len(st.session_state['height']) > 1:
-                st.write("Number of Pixel Clicked: ", len(st.session_state['height'])-1)
-                st.write("Points Selected: ", st.session_state['height'][1:])
+        # value = streamlit_image_coordinates
+        # mode = 'height'
+        # mode_selected = 'Height'
+        two_points_calculation(value, ratio, 'height', 'Height', 'calculated_values') 
 
 
-    elif mode == "Tilt Angle":
+    elif mode_selected == "Tilt Angle":
         st.session_state['width'] = []
         st.session_state['cum_height'] = []
         st.session_state['height'] = []
@@ -204,71 +227,21 @@ if uploadted_file is not None:
         st.write("🖐️ Click on the image to select three points for the angle.")
         st.write("🖐️ Click Order: BOTTOM - STRAIGHT TOP - TILTED TOP")
 
-        angle_reset = st.button("RESET")
-        message_angle_reset = st.empty()
-
-        if angle_reset:
-            st.session_state['angle'] = []
-            message_angle_reset.write("Resetting")
-
-            time.sleep(2)
-
-            message_angle_reset.empty()
-            angle_reset = False
-
-        if value and not angle_reset:
-
-            if len(st.session_state['angle']) < 4:
-                st.session_state['angle'].append([value['x'], value['y']])
-
-            if len(st.session_state['angle']) == 4:
-                pix_1 = st.session_state['angle'][-3]
-                pix_2 = st.session_state['angle'][-2]
-                pix_3 = st.session_state['angle'][-1]
-                st.session_state['calculated_values']['angle'] = angle_calculation(pix_1, pix_2, pix_3)
-                st.write("Trunk Tilt Angle: ", f"{st.session_state['calculated_values']['angle']} degrees")
-                st.write("Angle calcuation is finished. Please press the RESET button to select new points.")
-
-            if len(st.session_state['angle']) > 1:
-                st.write("Number of Pixel Clicked: ", len(st.session_state['angle'])-1)
-                st.write("Points Selected: ", st.session_state['angle'][1:])
+        # value = streamlit_image_coordinates
+        # mode = 'angle'
+        # mode_selected = 'Tilt Angle'
+        three_points_angle_calculation(value, 'angle', 'Tilt Angle', 'calculated_values')
     
-    elif mode == "Leaf Tilt Angle":
+    elif mode_selected == "Leaf Tilt Angle":
         st.session_state['width'] = []
         st.session_state['cum_height'] = []
         st.session_state['height'] = []
         st.session_state['angle'] = []
 
         st.write("🖐️ Click on the image to select three points for the leaf angle.")
-        st.write("🖐️ Click Order: BOTTOM - STRAIGHT TOP - TILTED TOP")
+        st.write("🖐️ Click Order: LEAF BOTTOM - LEAF BOTTOM - TRUNK")
 
-        leaf_angle_reset = st.button("RESET")
-        message_leaf_angle_reset = st.empty()
-
-        if leaf_angle_reset:
-            st.session_state['leaf_angle'] = []
-            message_leaf_angle_reset.write("Resetting")
-
-            time.sleep(2)
-
-            message_leaf_angle_reset.empty()
-            angle_reset = False
-
-        if value and not leaf_angle_reset:
-
-            if len(st.session_state['leaf_angle']) < 4:
-                st.session_state['leaf_angle'].append([value['x'], value['y']])
-
-            if len(st.session_state['leaf_angle']) == 4:
-                pix_1 = st.session_state['leaf_angle'][-3]
-                pix_2 = st.session_state['leaf_angle'][-2]
-                pix_3 = st.session_state['leaf_angle'][-1]
-                st.session_state['calculated_values']['leaf_angle'] = angle_calculation(pix_1, pix_2, pix_3)
-                st.write("Leaf Tilt Angle: ", f"{st.session_state['calculated_values']['leaf_angle']} degrees")
-                st.write("Leaf Angle calcuation is finished. Please press the RESET button to select new points.")
-
-            if len(st.session_state['leaf_angle']) > 1:
-                st.write("Number of Pixel Clicked: ", len(st.session_state['leaf_angle'])-1)
-                st.write("Points Selected: ", st.session_state['leaf_angle'][1:])
-
-
+        # value = streamlit_image_coordinates
+        # mode = 'leaf_angle'
+        # mode_selected = 'Leaf Tilt Angle'
+        three_points_angle_calculation(value, 'leaf_angle', 'Leaf Tilt Angle', 'calculated_values')
